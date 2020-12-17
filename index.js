52aa53b965b7a16cc6d99a9499becf235608525f
dad <60132399+dad2vad@users.noreply.github.com>
2020-12-17 12:27:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { handleEvent } from "flareact";
 /**
  * The DEBUG flag will do two things that help during development:
  * 1. we will skip caching on the edge, which makes it easier to
- *    debug.
+ *    debug
  * 2. we will return an error message on exception in your Response rather
  *    than the default 404.html page.
  */
