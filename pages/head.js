473213520b5d36b420e6652987444a336128b50d
dad <60132399+dad2vad@users.noreply.github.com>
2021-01-13 16:26:43 +0200

    Create head.js

---
+++ pages/head.js
@@ -0,0 +1,12 @@
+import Head from "flareact/head";
+
+export default function Index() {
+  return (
+    <div>
+      <Head>
+        <title>My page title</title>
+      </Head>
+      <h1>Hello, world.</h1>
+    </div>
+  );
+}
