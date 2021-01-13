65a87de9a0517b2bc7ccdb4e525634abbbfe1a47
dad <60132399+dad2vad@users.noreply.github.com>
2021-01-13 16:26:09 +0200

    Update and rename news.js to link.js

---
+++ pages/link.js
@@ -0,0 +1,11 @@
+import Link from "flareact/link";
+
+export default function Index() {
+  return (
+    <div>
+      <Link href="/about">
+        <a>Go to About</a>
+      </Link>
+    </div>
+  );
+}
